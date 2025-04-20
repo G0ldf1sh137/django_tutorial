a0278a0c64de7e8ad07a7d3f956ab3c112193de8
Matt Rich <m.rich137@gmail.com>
2025-04-20 09:40:06 -0400

    updated styles and started chores_day.html

--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -21,7 +21,6 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('', views.homepage),
     path('about/', views.about),
     path('posts/', include('posts.urls')),
@@ -29,6 +28,7 @@ urlpatterns = [
     path('polls/', include('polls.urls')),
     path('chores/', include('chores.urls')),
     path("__reload__/", include("django_browser_reload.urls")),
+    path('admin/', admin.site.urls),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
